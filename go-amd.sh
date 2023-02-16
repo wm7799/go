cb6575d6f8a692021800c7c549ca552af70cc89d
wm7799 <98514836+wm7799@users.noreply.github.com>
2023-02-16 13:02:23 +0800

    Update go-amd.sh

--- go-amd.sh
+++ go-amd.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
+sudo rm -rf /usr/local/go
+sudo apt-get remove golang -y
+sudo apt-get remove golang-go -y
+sudo apt-get autoremove -y
 
-#选择版本 (可去 https://golang.org/dl/ 地址查找安装包)
-VERSION=go1.20.1                      #此变量可选修改，安装包的版本号
-PKG_NAME=go1.20.1.linux-amd64.tar.gz  #此变量可选修改，安装包的名称
-
-#目录设置
-GO_DIR=/usr/local      #此变量可选修改，选择 Golang 安装的目录
-WORK_DIR=~/go_project  #此变量可选修改，选择日常的工作目录
-
-#下载安装
-URL=https://go.dev/dl/$PKG_NAME
-cd $GO_DIR && mkdir -p gopkg/$VERSION && cd gopkg && wget $URL
-tar -xzvf $PKG_NAME -C $VERSION --strip-components 1
-mv $VERSION ../go
-
-#创建工作目录
-mkdir -p  $WORK_DIR && cd $WORK_DIR && mkdir bin pkg src
+#下载文件
+sudo wget https://golang.google.cn/dl/go1.18.5.linux-amd64.tar.gz
+# 解压文件
+sudo tar xfz go1.18.5.linux-amd64.tar.gz -C /usr/local
 
 #设置环境变量
-echo "#Go Env Settings" >> ~/.bash_profile
-echo "export GOPATH=$WORK_DIR" >> ~/.bash_profile
-echo "export GOROOT=$GO_DIR/go" >> ~/.bash_profile
-echo "export GOBIN=\$GOPATH/bin" >> ~/.bash_profile
-echo "export PATH=\$PATH:\$GOBIN:\$GOROOT/bin" >> ~/.bash_profile 
+
+echo "export GOROOT=/usr/local/go" >> /etc/profile
+echo "export GOPATH=$HOME/gowork" >> /etc/profile
+echo "export GOBIN=$GOPATH/bin" >> /etc/profile
+echo "export PATH=$GOPATH:$GOBIN:$GOROOT/bin:$PATH" >> /etc/profile
+
+#刷新环境变量
+source /etc/profile
+#输入查看是否生效
+go version

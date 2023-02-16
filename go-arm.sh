060e5e578be75db5ac6fd6a51fe6b27278a5b32d
wm7799 <98514836+wm7799@users.noreply.github.com>
2023-02-16 13:07:34 +0800

    Update go-arm.sh

--- go-arm.sh
+++ go-arm.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-  
-
 #选择版本( 查找去 https://golang.org/dl/ )
 
-VERSION=go1.20.1
+sudo rm -rf /usr/local/go
+sudo apt-get remove golang -y
+sudo apt-get remove golang-go -y
+sudo apt-get autoremove -y
 
-PKG_NAME=go1.20.1.linux-arm64.tar.gz
+#下载文件
+sudo wget https://golang.google.cn/dl/go1.20.1.linux-arm64.tar.gz
+# 解压文件
+sudo tar xfz go1.20.1.linux-arm64.tar.gz -C /usr/local
 
-#go1.20.1.linux-arm64.tar.gz
-#安装目录
+#设置环境变量
 
-WORK_DIR=/usr/local
+echo "export GOROOT=/usr/local/go" >> /etc/profile
+echo "export GOPATH=$HOME/gowork" >> /etc/profile
+echo "export GOBIN=$GOPATH/bin" >> /etc/profile
+echo "export PATH=$GOPATH:$GOBIN:$GOROOT/bin:$PATH" >> /etc/profile
 
-#下载安装
-
-URL=https://go.dev/dl/$PKG_NAME
-
-cd $WORK_DIR && mkdir -p gopkg/$VERSION && cd gopkg &&wget $URL
-
-tar -xzvf $PKG_NAME -C $VERSION --strip-components 1
-
-rm -rf ../go &&mv $VERSION ../go
+#刷新环境变量
+source /etc/profile
+#输入查看是否生效
+go version

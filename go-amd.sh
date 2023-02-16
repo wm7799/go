9d4bb6dbad05889276c850941f7cc47eb1ef6f95
wm7799 <98514836+wm7799@users.noreply.github.com>
2023-02-16 11:56:48 +0800

    Update and rename go.sh to go-amd.sh

--- go-amd.sh
+++ go-amd.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
   
 #选择版本( 查找去 https://golang.org/dl/ )
-VERSION=go1.16.2
-PKG_NAME=go1.16.2.darwin-amd64.tar.gz
+VERSION=go1.20.1
+PKG_NAME=go1.20.1.linux-amd64.tar.gz
 
 #安装目录
 WORK_DIR=/usr/local
 
 #下载安装
-URL=https://dl.google.com/go/$PKG_NAME
+URL=https://go.dev/dl/$PKG_NAME
 cd $WORK_DIR && mkdir -p gopkg/$VERSION && cd gopkg &&wget $URL
 tar -xzvf $PKG_NAME -C $VERSION --strip-components 1
 rm -rf ../go &&mv $VERSION ../go
